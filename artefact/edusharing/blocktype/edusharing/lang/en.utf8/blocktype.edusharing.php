03b00b190a53338f32f36f1f9e7714ce402db318
chr-ernst <ernst@edu-sharing.net>
2019-07-16 14:48:49 +0200

    i18n: added en for dialog fix: gitignore settings

---
+++ artefact/edusharing/blocktype/edusharing/lang/en.utf8/blocktype.edusharing.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage blocktype-plans
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'edu-sharing';
+$string['defaulttitledescription'] = '';//'If this is left empty, the title of object will be used.';
+$string['eduversionshow'] = 'Version';
+$string['eduversionshow_current'] = 'exactly this';
+$string['eduversionshow_last'] = 'always newest';
+
+$string['eduversionshow_title'] = 'Title';
+$string['eduversionshow_width'] = 'Width';
+$string['eduversionshow_height'] = 'Height';
+$string['eduversionshow_alignment'] = 'Placement';
+$string['eduversionshow_alignment_inline'] = 'in text';
+$string['eduversionshow_alignment_left'] = 'left';
+$string['eduversionshow_alignment_right'] = 'right';
+$string['eduversionshow_alignment_block'] = 'block element';
+$string['edudirectory1'] = 'Materials will be added to the folder and published.';
+$string['edudirectory2'] = 'Subfolders will not be included.';
