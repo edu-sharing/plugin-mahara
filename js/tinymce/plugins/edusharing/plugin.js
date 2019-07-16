cff6f288c297920fadc953dd5e8c77d80468356d
chr-ernst <ernst@edu-sharing.net>
2019-07-16 13:19:10 +0200

    Remove edusharing-butto from forum posts

--- js/tinymce/plugins/edusharing/plugin.js
+++ js/tinymce/plugins/edusharing/plugin.js
@@ -11,90 +11,93 @@ tinymce.PluginManager.add('edusharing', function(editor, url) {
     })
 
     // Add a button that opens a window
-    editor.addButton('edusharing', {
-        image: config.wwwroot+'/artefact/edusharing/img/edusharing.png',
-        onclick: function() {
+    if(!window.location.href.includes('/forum/')){ //do not include button in forums since we cant set a usage there
+        editor.addButton('edusharing', {
+            image: config.wwwroot+'/artefact/edusharing/img/edusharing.png',
+            onclick: function() {
 
-            if(editor.selection.getNode().classList.contains('edusharingObject') || editor.selection.getNode().parentNode.classList.contains('edusharingObject'))
-                console.log('edit mode not implemented');
-            else
-            // Open window
-            editor.windowManager.open({
-                title: 'Material aus edu-sharing einfügen',
-                url: url + '/dialog.php',
-                width: 950,
-                height: 600,
-                buttons: [
-                    {
-                        text: 'Insert',
-                        onclick: function() {
-                            // Top most window object
-                            var win = editor.windowManager.getWindows()[0];
-                            var node = JSON.parse(win.getContentWindow().document.getElementById('eduNode').value);
+                if(editor.selection.getNode().classList.contains('edusharingObject') || editor.selection.getNode().parentNode.classList.contains('edusharingObject'))
+                    console.log('edit mode not implemented');
+                else
+                // Open window
+                    editor.windowManager.open({
+                        title: 'Material aus edu-sharing einfügen',
+                        url: url + '/dialog.php',
+                        width: 950,
+                        height: 600,
+                        buttons: [
+                            {
+                                text: 'Insert',
+                                onclick: function() {
+                                    // Top most window object
+                                    var win = editor.windowManager.getWindows()[0];
+                                    var node = JSON.parse(win.getContentWindow().document.getElementById('eduNode').value);
 
-                            // Insert the contents of the dialog.html textarea into the editor
-                            var title = node.title || node.name;
-                            var version = win.getContentWindow().document.getElementsByName('eduFormVersion');
-                            for (var i = 0, length = version.length; i < length; i++) {
-                                if (version[i].checked) {
-                                    version = version[i].value;
-                                    break;
-                                }
-                            }
-                            var alignments = win.getContentWindow().document.getElementsByName('eduFormAlignment');
-                            for (var i = 0, length = alignments.length; i < length; i++) {
-                                if (alignments[i].checked) {
-                                    var alignment = alignments[i].value;
-                                    switch(alignments[i].value) {
-                                        case 'left':
-                                            var style='style="float:left"';
+                                    // Insert the contents of the dialog.html textarea into the editor
+                                    var title = node.title || node.name;
+                                    var version = win.getContentWindow().document.getElementsByName('eduFormVersion');
+                                    for (var i = 0, length = version.length; i < length; i++) {
+                                        if (version[i].checked) {
+                                            version = version[i].value;
                                             break;
-                                        case 'right':
-                                            var style='style="float:left"';
-                                            break;
-                                        case 'block':
-                                            var style='style="display:block"';
-                                            break;
-                                        default:
-                                            var style='';
+                                        }
                                     }
-                                    break;
+                                    var alignments = win.getContentWindow().document.getElementsByName('eduFormAlignment');
+                                    for (var i = 0, length = alignments.length; i < length; i++) {
+                                        if (alignments[i].checked) {
+                                            var alignment = alignments[i].value;
+                                            switch(alignments[i].value) {
+                                                case 'left':
+                                                    var style='style="float:left"';
+                                                    break;
+                                                case 'right':
+                                                    var style='style="float:left"';
+                                                    break;
+                                                case 'block':
+                                                    var style='style="display:block"';
+                                                    break;
+                                                default:
+                                                    var style='';
+                                            }
+                                            break;
+                                        }
+                                    }
+
+                                    var srcUrl = node.preview.url;
+                                    srcUrl += '&objectId=' + node.ref.id;
+                                    srcUrl += '&objectUrl=' + node.objectUrl;
+                                    srcUrl += '&title=' + title;
+                                    srcUrl += '&mediatype=' + node.mediatype;
+                                    srcUrl += '&mimetype=' + node.mimetype;
+                                    srcUrl += '&contentVersion=' + node.contentVersion;
+                                    srcUrl += '&version=' + version;
+                                    srcUrl += '&alignment=' + alignment;
+                                    srcUrl += '&alt=' + title;
+
+                                    if(node.mediatype.indexOf('image') !== -1 || node.mediatype.indexOf('video') !== -1 || node.mediatype.indexOf('h5p') !== -1 || node.mediatype.indexOf('folder') !== -1) {
+                                        srcUrl += '&width=' + win.getContentWindow().document.getElementById('eduFormWidth').value;
+                                        srcUrl += '&height=' + win.getContentWindow().document.getElementById('eduFormHeight').value;
+                                        editor.insertContent ('<img '+style+' src="'+srcUrl+'" ' +
+                                            'class="edusharingObject" ' +
+                                            '/>');
+                                    } else {
+                                        editor.insertContent ('<a '+ style +
+                                            'class="edusharingObject" href="'+srcUrl+'">' +
+                                            title + '</a>');
+                                    }
+
+                                    // Close the window
+                                    win.close();
                                 }
-                            }
+                            },
 
-                            var srcUrl = node.preview.url;
-                            srcUrl += '&objectId=' + node.ref.id;
-                            srcUrl += '&objectUrl=' + node.objectUrl;
-                            srcUrl += '&title=' + title;
-                            srcUrl += '&mediatype=' + node.mediatype;
-                            srcUrl += '&mimetype=' + node.mimetype;
-                            srcUrl += '&contentVersion=' + node.contentVersion;
-                            srcUrl += '&version=' + version;
-                            srcUrl += '&alignment=' + alignment;
-                            srcUrl += '&alt=' + title;
+                            {text: 'Close', onclick: 'close'}
+                        ]
+                    });
+            }
+        });
+    }
 
-                            if(node.mediatype.indexOf('image') !== -1 || node.mediatype.indexOf('video') !== -1 || node.mediatype.indexOf('h5p') !== -1 || node.mediatype.indexOf('folder') !== -1) {
-                                srcUrl += '&width=' + win.getContentWindow().document.getElementById('eduFormWidth').value;
-                                srcUrl += '&height=' + win.getContentWindow().document.getElementById('eduFormHeight').value;
-                                editor.insertContent ('<img '+style+' src="'+srcUrl+'" ' +
-                                    'class="edusharingObject" ' +
-                                    '/>');
-                            } else {
-                                editor.insertContent ('<a '+ style +
-                                    'class="edusharingObject" href="'+srcUrl+'">' +
-                                    title + '</a>');
-                            }
-
-                            // Close the window
-                            win.close();
-                        }
-                    },
-
-                    {text: 'Close', onclick: 'close'}
-                ]
-            });
-        }
-    });
 /*
     // Adds a menu item to the tools menu
     editor.addMenuItem('edusharing', {
